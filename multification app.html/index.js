6936e4a67a00e52636465022ce54e22f4eef4e1b
rssharna <92268184+rssharna@users.noreply.github.com>
2023-11-28 13:42:18 +0600

    Update index.js

--- multification app.html/index.js
+++ multification app.html/index.js
@@ -1,5 +1,5 @@
 const num1 = Math.ceil(Math.random()*10);
-const num2 = Math.ceil(Math,random()*10);
+const num2 = Math.ceil(Math.random()*10);
 
 const questionE1 = document.getElementById(question);
 
