cae23a6c9b2b066f8ff346b63325b93876ba2b13
rssharna <92268184+rssharna@users.noreply.github.com>
2023-11-28 14:16:41 +0600

    Update index.js

--- practice.html/index.js
+++ practice.html/index.js
@@ -1,30 +1,15 @@
-const hourEl =document.getElementById("hour");
-const minuteEl = document.getElementById("minutes");
-const secondEl = document.getElementById("seconds");
-const ampmEl = document.getElementById("ampm");
-
-function updateClock() {
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-    let ampm = "AM";
-
-     if(h > 12) {
-        h = h - 12;
-        ampm = "PM";
-     }
-
-     h = (h < 10) ? "0" + h : h;
-     m = (m < 10) ? "0" + m : m;
-     s = (s < 10) ? "0" + s : s;
-
-     hourEl.innerText = h;
-     minuteEl.innerText = m;
-     secondEl.innerText = s;
-     ampmEl,innerText = ampm;
-     setTimeout(() => {
-        updateClock();
-     },1000 );
-
-}
-updateClock();
+function startTime() {
+   const today = new Date();
+   let h = today.getHours();
+   let m = today.getMinutes();
+   let s = today.getSeconds();
+   m = checkTime(m);
+   s = checkTime(s);
+   document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+   setTimeout(startTime, 1000);
+ }
+ 
+ function checkTime(i) {
+   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+   return i;
+ }
